a74f5c7ab23f792afcd40beed32adc8ea6cb2e49
Preetam Raj <iitianpreetam@gmail.com>
2021-12-04 03:34:24 +0530

    added allowed hosts

--- foe_gb_api/settings.py
+++ foe_gb_api/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foe-gb-api.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
