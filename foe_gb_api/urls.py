3c740676db22edc9375496564d52f01ec6db20fe
Preetam Raj <iitianpreetam@gmail.com>
2021-12-04 03:08:21 +0530

    initial change

---
+++ foe_gb_api/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('gb_app.urls')),
+    path('api/', include('gb_app_api.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
